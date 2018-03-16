d376595f3a2a8c14831c82a35389fa17c6789fe8
jose jesus leyva orozco <naniz_leyva@outlook.com>
2018-03-15 21:32:11 -0600

    Funcion de Regitrar Usuario
    Se creo la funcion en el documento app.js

---
+++ LoginFirebase/app.js
@@ -0,0 +1,16 @@
+function Registrar()
+{
+    var email = document.getElementById('email').value;
+    var contrasena = document.getElementById('contrasena').value;
+
+    firebase.auth().createUserWithEmailAndPassword(email, contrasena)
+    .catch(function(error) {
+        // Handle Errors here.
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        console.log(errorCode);
+        console.log(errorMessage);
+        // ...
+      });
+      
+}
